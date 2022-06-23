c0e0f1b1bc39f13445d6adb60714441ae7fb3e7b
taylorbohl <tbohl3@gmail.com>
2022-06-22 20:04:37 -0500

    updating py scripts

--- tornado_pull.py
+++ tornado_pull.py
@@ -4,43 +4,41 @@ import requests
 import datetime 
 
 def pull():
+    # Code for 60 days' retro data
+    today = pd.to_datetime("today") + datetime.timedelta(days=1)  
 
-    #Start Dates
-    start_date = datetime.date(2010, 1, 1)
-    end_date = datetime.date(2020, 12, 1)
-    delta = datetime.timedelta(days=25)
-
-    start_list =[]
-
-    while start_date <= end_date:
-        start_list.append(start_date)
-        start_date += delta
-
-    #End Dates
-    start_date = datetime.date(2010, 1, 26)
-    end_date = datetime.date(2021, 12, 1)
-    delta = datetime.timedelta(days=25)
-
-    end_list =[]
-
-    while start_date <= end_date:
-        end_list.append(start_date)
-        start_date += delta
-
-    #Converting lists to strings 
+    start_str_list = []
     end_str_list = []
-    for i in range(0,len(end_list)):
-        end_str_list.append(end_list[i].strftime('%Y%m%d'))
-        
-    start_str_list =[]
-    for i in range(0,len(start_list)):
-        start_str_list.append(start_list[i].strftime('%Y%m%d'))
+
+    retro_start = 60
+    retro_end = 30
+
+    # For start dates
+    for x in range(0,2):
+        if retro_start >= 0:
+            start = today - datetime.timedelta(days=retro_start)
+            start_str_list.append(start.strftime('%Y%m%d'))
+            retro_start -=30
+            
+        else:
+            pass
+
+    # For end dates
+    for y in range(0,1):
+        if retro_end > 0:
+            end = today - datetime.timedelta(days=retro_end)
+            end_str_list.append(end.strftime('%Y%m%d'))        
+            retro_end -=30
+            
+        else:
+            pass
+
+    end_str_list.append(today.strftime('%Y%m%d'))
 
     #List of endpoints for API
     complete_endpoints = []
     for i in range(0,len(start_str_list)):
         complete_endpoints.append(f"{start_str_list[i]}:{end_str_list[i]}")
-
 
     #Base URL setup
     base_url = "https://www.ncdc.noaa.gov/swdiws/json/nx3tvs/"
@@ -50,7 +48,6 @@ def pull():
     for dates in complete_endpoints:
         complete_url = base_url + dates
         all_urls.append(complete_url)
-
 
     #List/Variables setup
     cell_type =[]
@@ -64,8 +61,6 @@ def pull():
     range_ = []
     lat = []
     lon = []
-
-
 
     #----BEGIN Loop----
     # Looping through urls and appending data to lists
@@ -83,7 +78,8 @@ def pull():
             range_.append(response["result"][i]["RANGE"])
             lon.append(response["result"][i]["SHAPE"].split()[1:][0].split('(')[1])
             lat.append(response["result"][i]["SHAPE"].split()[1:][1].split(')')[0])
-    #----END Loop----
+
+    #----END Loop-----
 
     #Creating dataframe of all records
     dict = {"Cell_Type": cell_type,
@@ -100,14 +96,17 @@ def pull():
         }
 
     df = pd.DataFrame(dict)
+    df
 
+    #Creating additional columns
     df['Date']=pd.to_datetime(df['zTime']).dt.date
     df['Time']=pd.to_datetime(df['zTime']).dt.time
     df['Year']=pd.to_datetime(df['zTime']).dt.year
     df['Month']=pd.to_datetime(df['zTime']).dt.month
 
 
-    df['PKID']=df['Cell_ID']+df['Year'].astype(str)+df['Month'].astype(str)
+    #Primary Key to id tornados
+    df['PKID']=df['Cell_ID']+df['zTime'].astype(str)
 
     #convert df back to dictionary for app.py
     tornado_dict = df.to_dict()
