69f648cd789a5268e3b37d54ca5b8b6ae13c03c9
clawson13 <clawson131@gmail.com>
2022-06-23 18:49:22 -0500

    Current visualizations thru morning 6-23

--- static/js/tracking.js
+++ static/js/tracking.js
@@ -1,133 +1,178 @@
 // Initialize the necessary LayerGroups
 let layers = {
-  quakes: new L.LayerGroup(),
-  plates: new L.LayerGroup()
+  markers: new L.LayerGroup()
+  // plates: new L.LayerGroup()
 };
 
-// Set data source for earthquake data
-const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+// Set data source for tornadoes data
+const queryUrl = "../../static/data/tornadoes.json"
+
+var twisterIcon = L.icon({
+  iconUrl: '../../static/img/twister.png',
+
+  iconSize:     [36, 24] // size of the icon
+  // shadowSize:   [50, 64], // size of the shadow
+  // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  // shadowAnchor: [4, 62],  // the same for the shadow
+  // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function(data) {
-  createFeatures(data.features);
+  createMarkers(data);
 });
 
-function createFeatures(earthquakeData) {
-
-  // Give each earthquake a popup describing the place, time, magnitude, and depth of the earthquake
-  function popUpMsg(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p> <b>Date/Time: </b>" + new Date(feature.properties.time) + "</p>" +
-        "<p> <b>Magnitude:</b> " + feature.properties.mag + "; <b>Depth:</b> " + feature.geometry.coordinates[2] + "</p>");
-    }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the popUpMsg function once for each piece of data in the array
-  let earthquakes = L.geoJSON(earthquakeData, {
-          pointToLayer: function (feature, latlng) {
-              if (feature.geometry.coordinates[2] >= 90) {
-                  return L.circleMarker(latlng, {
-                    radius: feature.properties.mag * 5,
-                    fillColor: "#E74C3C",
-                    color: "black",
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8
-                  });
-            
-                } else if (feature.geometry.coordinates[2] >= 70) {
-                  return L.circleMarker(latlng, {
-                    radius: feature.properties.mag * 5,
-                    fillColor: "#DC7633",
-                    color: "black",
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8
-                  });
-            
-                } else if (feature.geometry.coordinates[2] >= 50) {
-                  return L.circleMarker(latlng, {
-                    radius: feature.properties.mag * 5,
-                    fillColor: "#F39C12",
-                    color: "black",
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8
-                  });
-                                      
-                } else if (feature.geometry.coordinates[2] >= 30) {
-                    return L.circleMarker(latlng, {
-                    radius: feature.properties.mag * 5,
-                    fillColor: "#F1C40F",
-                    color: "black",
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8
-                  });
-                                      
-                } else if (feature.geometry.coordinates[2] >= 10) {
-                    return L.circleMarker(latlng, {
-                    radius: feature.properties.mag * 5,
-                    fillColor: "#DFFF00",
-                    color: "black",
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8
-                  });
-              
-                } else {
-                    return L.circleMarker(latlng, {
-                    radius: feature.properties.mag * 5,
-                    fillColor: "#2ECC71",
-                    color: "black",
-                    weight: 1,
-                    opacity: 1,
-                    fillOpacity: 0.8
-                  });
-                } 
-          },
-          onEachFeature: popUpMsg
-      });
-  
-  // Add to the Earthquakes layer
-  earthquakes.addTo(layers.quakes);
-};
-
-// Set data source for techtonic data
-const tectUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
-
-// Perform a GET request to the query URL/
-d3.json(tectUrl).then(function (data) {
-  // Once we get a response, send the data.features object to the createTects function
-  createTects(data.features);
-});
-
-function createTects(tectData) {
-
-  // Create a GeoJSON layer that contains the features array on the tectData object
-  // Assign color and set fill opacity to 0
-  var tectplates = L.geoJSON(tectData, {
-    onEachFeature: function (feature, layer) {
-      layer.bindTooltip(feature.geometry).setStyle({color :'#FFA500', fillOpacity: 0})
-  }});
-
-  // Add to the Tectonic Plates layer
-  tectplates.addTo(layers.plates);
-}
-
-// Create the base layers
-let satelliteMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-    maxZoom: 18
+function createMarkers(data) {
+  for (var i = 0; i < 100; i++) {
+  //   let point = L.circle([data[i].Lat, data[i].Lon], {
+  //     color: "red",
+  //     fillColor: "red",
+  //     fillOpacity: 0.75,
+  //     radius: 10
+  //   })
+    let direction = parseFloat(data[i].Azimuth);
+    
+    let point = L.marker(L.latLng(data[i].Lat, data[i].Lon), {
+      // radius: 10,
+      icon: twisterIcon,
+      rotationAngle: direction
   });
 
-let grayMap = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+    point.bindPopup("<h3>" + data[i].Shape +
+        "</h3><hr><p> <b>Date/Time: </b>" + data[i].zTime + "</p>" +
+        "<p> <b>Angle:</b> " + data[i].Azimuth + "; <b>MXDV:</b> " + data[i].MXDV + "</p>");
+
+    point.addTo(layers.markers);
+}
+}
+
+
+// }
+
+// // Set data source for earthquake data
+// const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// // Perform a GET request to the query URL/
+// d3.json(queryUrl).then(function(data) {
+//   createFeatures(data.features);
+// });
+
+// function createFeatures(earthquakeData) {
+
+//   // Give each earthquake a popup describing the place, time, magnitude, and depth of the earthquake
+//   function popUpMsg(feature, layer) {
+//     layer.bindPopup("<h3>" + feature.properties.place +
+//         "</h3><hr><p> <b>Date/Time: </b>" + new Date(feature.properties.time) + "</p>" +
+//         "<p> <b>Magnitude:</b> " + feature.properties.mag + "; <b>Depth:</b> " + feature.geometry.coordinates[2] + "</p>");
+//     }
+
+//   // Create a GeoJSON layer containing the features array on the earthquakeData object
+//   // Run the popUpMsg function once for each piece of data in the array
+//   let earthquakes = L.geoJSON(earthquakeData, {
+//           pointToLayer: function (feature, latlng) {
+//               if (feature.geometry.coordinates[2] >= 90) {
+//                   return L.circleMarker(latlng, {
+//                     radius: feature.properties.mag * 5,
+//                     fillColor: "#E74C3C",
+//                     color: "black",
+//                     weight: 1,
+//                     opacity: 1,
+//                     fillOpacity: 0.8
+//                   });
+            
+//                 } else if (feature.geometry.coordinates[2] >= 70) {
+//                   return L.circleMarker(latlng, {
+//                     radius: feature.properties.mag * 5,
+//                     fillColor: "#DC7633",
+//                     color: "black",
+//                     weight: 1,
+//                     opacity: 1,
+//                     fillOpacity: 0.8
+//                   });
+            
+//                 } else if (feature.geometry.coordinates[2] >= 50) {
+//                   return L.circleMarker(latlng, {
+//                     radius: feature.properties.mag * 5,
+//                     fillColor: "#F39C12",
+//                     color: "black",
+//                     weight: 1,
+//                     opacity: 1,
+//                     fillOpacity: 0.8
+//                   });
+                                      
+//                 } else if (feature.geometry.coordinates[2] >= 30) {
+//                     return L.circleMarker(latlng, {
+//                     radius: feature.properties.mag * 5,
+//                     fillColor: "#F1C40F",
+//                     color: "black",
+//                     weight: 1,
+//                     opacity: 1,
+//                     fillOpacity: 0.8
+//                   });
+                                      
+//                 } else if (feature.geometry.coordinates[2] >= 10) {
+//                     return L.circleMarker(latlng, {
+//                     radius: feature.properties.mag * 5,
+//                     fillColor: "#DFFF00",
+//                     color: "black",
+//                     weight: 1,
+//                     opacity: 1,
+//                     fillOpacity: 0.8
+//                   });
+              
+//                 } else {
+//                     return L.circleMarker(latlng, {
+//                     radius: feature.properties.mag * 5,
+//                     fillColor: "#2ECC71",
+//                     color: "black",
+//                     weight: 1,
+//                     opacity: 1,
+//                     fillOpacity: 0.8
+//                   });
+//                 } 
+//           },
+//           onEachFeature: popUpMsg
+//       });
+  
+//   // Add to the Earthquakes layer
+//   earthquakes.addTo(layers.quakes);
+// };
+
+// // Set data source for techtonic data
+// const tectUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
+
+// // Perform a GET request to the query URL/
+// d3.json(tectUrl).then(function (data) {
+//   // Once we get a response, send the data.features object to the createTects function
+//   createTects(data.features);
+// });
+
+// function createTects(tectData) {
+
+//   // Create a GeoJSON layer that contains the features array on the tectData object
+//   // Assign color and set fill opacity to 0
+//   var tectplates = L.geoJSON(tectData, {
+//     onEachFeature: function (feature, layer) {
+//       layer.bindTooltip(feature.geometry).setStyle({color :'#FFA500', fillOpacity: 0})
+//   }});
+
+//   // Add to the Tectonic Plates layer
+//   tectplates.addTo(layers.plates);
+// }
+
+// Create the base layers
+let grayScale = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   maxZoom: 18,
   subdomains: 'abcd',
   minZoom: 0,
   ext: 'png'
 });
+
+let streetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 18
+  });
 
 let topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
@@ -136,22 +181,22 @@ let topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 
 // Define a baseMaps object to hold our base layers
 let baseMaps = {
-    "Satellite" : satelliteMap,
-    "Grayscale": grayMap,
+    "Grayscale" : grayScale,
+    "Street" : streetMap,
     "Topographic" : topoMap
     };
 
 // Create an overlay object to hold our overlay
 let overlayMaps = {
-  "Tectonic Plates": layers.plates,
-  "Earthquakes": layers.quakes
+  "Tornado Paths": layers.markers
+  // "Earthquakes": layers.quakes
 };
 
 // Create our map, giving it the satellite base and earthquake/plates layers to display on load
 let myMap = L.map("map", {
     center: [ 37.09, -95.71 ],
     zoom: 5,
-    layers: [satelliteMap, layers.plates]
+    layers: [grayScale, layers.markers]
 });
 
 // Create a layer control
@@ -187,3 +232,62 @@ legend.onAdd = function (myMap) {
       };
 
 legend.addTo(myMap);
+
+
+(function() {
+  // save these original methods before they are overwritten
+  var proto_initIcon = L.Marker.prototype._initIcon;
+  var proto_setPos = L.Marker.prototype._setPos;
+
+  var oldIE = (L.DomUtil.TRANSFORM === 'msTransform');
+
+  L.Marker.addInitHook(function () {
+      var iconOptions = this.options.icon && this.options.icon.options;
+      var iconAnchor = iconOptions && this.options.icon.options.iconAnchor;
+      if (iconAnchor) {
+          iconAnchor = (iconAnchor[0] + 'px ' + iconAnchor[1] + 'px');
+      }
+      this.options.rotationOrigin = this.options.rotationOrigin || iconAnchor || 'center bottom' ;
+      this.options.rotationAngle = this.options.rotationAngle || 0;
+
+      // Ensure marker keeps rotated during dragging
+      this.on('drag', function(e) { e.target._applyRotation(); });
+  });
+
+  L.Marker.include({
+      _initIcon: function() {
+          proto_initIcon.call(this);
+      },
+
+      _setPos: function (pos) {
+          proto_setPos.call(this, pos);
+          this._applyRotation();
+      },
+
+      _applyRotation: function () {
+          if(this.options.rotationAngle) {
+              this._icon.style[L.DomUtil.TRANSFORM+'Origin'] = this.options.rotationOrigin;
+
+              if(oldIE) {
+                  // for IE 9, use the 2D rotation
+                  this._icon.style[L.DomUtil.TRANSFORM] = 'rotate(' + this.options.rotationAngle + 'deg)';
+              } else {
+                  // for modern browsers, prefer the 3D accelerated version
+                  this._icon.style[L.DomUtil.TRANSFORM] += ' rotateZ(' + this.options.rotationAngle + 'deg)';
+              }
+          }
+      },
+
+      setRotationAngle: function(angle) {
+          this.options.rotationAngle = angle;
+          this.update();
+          return this;
+      },
+
+      setRotationOrigin: function(origin) {
+          this.options.rotationOrigin = origin;
+          this.update();
+          return this;
+      }
+  });
+})();
